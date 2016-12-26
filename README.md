0a69c1249da18b89e36cb8e6d244719f14c727c9
Leroy Zhang <leroy.zhang@outlook.com>
2016-12-27 00:02:41 +0800

    20161227 update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CrowdSourcing
+#CrowdSourcing
 the first crowdsourcing project for collecting brilliant business ideas and engaging co-creation
---
+++ test.py
@@ -0,0 +1,8 @@
+#! /use/bin/env python
+#coding:utf-8
+age = input("How old are you?")
+name = input("what is your name?")
+after_ten = int(age) + 10
+print ("You are " + age + " years old.")
+print ("Your name is:", name)
+print("You will be "+ str(after_ten)+ "years old after ten years")
